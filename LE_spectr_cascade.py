be1aabc0a7399305865464f79eb1f77edff6d700
davniki <davniki@outlook.com>
2025-01-14 13:07:40 +0300

    changing in the fase_port.py , LE_spectr_cascade.py

--- LE_spectr_cascade.py
+++ LE_spectr_cascade.py
@@ -1,21 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sys import stdout
 from mpmath import mp
 
 mp.dps = 30
 # Спектр ляпуновских показателей
-
-A = mp.mpf('1.43')
-B = mp.mpf('0.5')
-C = mp.mpf('-1.758')
-
-m = 1000
-t = 10
-
-L_1 = []
-L_2 = []
-L_3 = []
-c = []
 
 
 def Henon(x, A, B, C):
@@ -34,7 +23,22 @@ def d_Henon(dx, x, A, B, C):
     return dx0
 
 
-for q in range(10):
+A = mp.mpf('1.991')
+B = mp.mpf('0.5')
+C = mp.mpf('-1.89')
+
+m = 1000
+t = 10
+
+L_1 = []
+L_2 = []
+L_3 = []
+c = []
+
+for q in range(100):
+    stdout.write("\r%d" % q)
+    stdout.flush()
+
     x = mp.matrix([[mp.mpf('0.1'), mp.mpf('0.1'), mp.mpf('0.1')]])
 
     # начальные вектора возмущения, которые ортоганальны и нормированы на еденицу
@@ -50,7 +54,7 @@ for q in range(10):
     g_2 = 0
     g_3 = 0
 
-    for l in range(300):
+    for l in range(100):
         x = Henon(x, A, B, C)
 
     for j in range(m):
@@ -80,17 +84,17 @@ for q in range(10):
     L_1.append(sum_1 / (g_1 * t))
     L_2.append(sum_2 / (g_2 * t))
     # L_3.append(sum_3 / (g_3 * t))
-    c.append(C)
-    C -= 0.0003
+    c.append(A)
+    A -= 0.000001
 
 # fp1 = np.linspace(-1.464, -1.464, 100) # NS
 # fp2 = np.linspace(-1.702, -1.702, 100) # C-4
 # fp3 = np.linspace(-1.7485, -1.7485, 100) # C-8
-fp4 = np.linspace(-1.75925, -1.75925, 100) # C-16
-#
+# fp4 = np.linspace(-1.75925, -1.75925, 100) # C-16
+
 # fp5 = np.linspace(-1.5228, -1.5228, 100) # C-32
 # fp6 = np.linspace(-1.527, -1.527, 100) # C-64
-fp7 = np.linspace(-1.7596, -1.7596, 100) # CH
+# fp7 = np.linspace(-1.7596, -1.7596, 100) # CH
 # fp8 = np.linspace(-1.619, -1.619, 100) # HC
 
 fig, ax = plt.subplots()
@@ -104,11 +108,11 @@ l2 = ax.plot(c, L_2, label='L-2')
 # line1, = ax.plot(fp1, a1, label='NS')
 # line2, = ax.plot(fp2, a1, label='ICD-1')
 # line3, = ax.plot(fp3, a1, label='ICD-2')
-line4, = ax.plot(fp4, a1, label='ICD-3')
+# line4, = ax.plot(fp4, a1, label='ICD-3')
 #
 # line5, = ax.plot(fp5, a1, label='C-32')
 # line6, = ax.plot(fp6, a1, label='C-64')
-line7, = ax.plot(fp7, a1, label='CH')
+# line7, = ax.plot(fp7, a1, label='CH')
 # line8, = ax.plot(fp8, a1, label='HC')
 
 # ax.annotate('NS', xy=(-1.47, 0), xytext=(-1.42, 0.1),
@@ -118,13 +122,13 @@ line7, = ax.plot(fp7, a1, label='CH')
 # ax.annotate('ICD-2', xy=(-1.7485, 0), xytext=(-1.72, 0.05),
 #             arrowprops=dict(facecolor='black', shrink=0.05))
 
-ax.annotate('ICD-3', xy=(-1.75925, 0), xytext=(-1.7588, 0.005),
-            arrowprops=dict(facecolor='black', shrink=0.05))
+# ax.annotate('ICD-3', xy=(-1.75925, 0), xytext=(-1.7588, 0.005),
+#             arrowprops=dict(facecolor='black', shrink=0.05))
 #
 # ax.annotate('C-32', xy=(-1.5228, 0), xytext=(-1.465, 0.066),
 #             arrowprops=dict(facecolor='black', shrink=0.05))
-ax.annotate('CH', xy=(-1.7596, 0), xytext=(-1.7592, 0.01),
-            arrowprops=dict(facecolor='black', shrink=0.05))
+# ax.annotate('CH', xy=(-1.7596, 0), xytext=(-1.7592, 0.01),
+#             arrowprops=dict(facecolor='black', shrink=0.05))
 # ax.annotate('HC', xy=(-1.619, 0), xytext=(-1.65, -0.066),
 #             arrowprops=dict(facecolor='black', shrink=0.05))
 # # ax.annotate('ICD-2', xy=(-1.759255, 0), xytext=(-1.725, 0.066),
@@ -133,8 +137,8 @@ ax.annotate('CH', xy=(-1.7596, 0), xytext=(-1.7592, 0.01),
 ax.grid()
 ax.legend()
 # ax.legend(handles=[line1, line2, line3, line4])
-ax.set_title(f"A = {A}, B = {B}")
-ax.set_xlabel("Параметр C")
+ax.set_title(f"C = {C}, B = {B}")
+ax.set_xlabel("Параметр A")
 ax.set_ylabel("Показатель Ляпунова L")
 
 plt.show()
--- fase_port.py
+++ fase_port.py
@@ -3,7 +3,7 @@ import numpy as np
 from sys import stdout
 
 from mpmath import mp
-mp.dps = 25
+mp.dps = 30
 
 ###
 #  Фазовый портрет динамической системы
@@ -22,9 +22,9 @@ def Henon(x, A, B, C):
 # b = mp.mpf('0.5')
 # c = mp.mpf('-1.7574339')
 
-a = mp.mpf('1.43')
+a = mp.mpf('1.990915')
 b = mp.mpf('0.5')
-c = mp.mpf('-1.7596')
+c = mp.mpf('-1.89')
 
 # x0 = 0.1
 # y0 = 0.2
@@ -39,21 +39,28 @@ fz = []
 fx1 = []
 fy1 = []
 fz1 = []
+
+fx2 = []
+fy2 = []
+fz2 = []
+
 T = []
+
 k = 0
 
-for i in range(10_000):
+for i in range(100):
     x = Henon(x, a, b, c)
-
-for i in range(1_000_000):
-    stdout.write("\r%d" % i)
-    stdout.flush()
-    x = Henon(x, a, b, c)
-
-    fx.append(x[0])
-    fy.append(x[1])
-    fz.append(x[2])
-    T.append(i)
+#
+# for i in range(10_000):
+#     stdout.write("\r%d" % i)
+#     stdout.flush()
+#     x = Henon(x, a, b, c)
+#
+#     fx.append(x[0])
+#     fy.append(x[1])
+#     fz.append(x[2])
+#
+#     k += int(1)
 #
 #     if (-0.393 < x[0] < -0.39) and (-0.4055 < x[1] < -0.4035):
 #         fx1.append(x[0])
@@ -66,9 +73,7 @@ for i in range(1_000_000):
 # b = mp.mpf('0.5')
 # c = mp.mpf('-1.757435')
 #
-# fx2 = []
-# fy2 = []
-# fz2 = []
+
 #
 # x = [mp.mpf('0.1'), mp.mpf('0.1'), mp.mpf('0.1')]
 #
@@ -81,64 +86,65 @@ for i in range(1_000_000):
 #     fy2.append(x[1])
 #     fz2.append(x[2])
 
-# m = 0
-# l =0
-#
-# while k != 250:
-#     stdout.write("\r%d" % k)
-#     stdout.flush()
-#
-#     x = Henon(x, a, b, c)
-#
-#     fx.append(x[0])
-#     fy.append(x[1])
-#     fz.append(x[2])
-#
-#     m += 1
-#     # print(m)
-#
-#     if (-0.393 < x[0] < -0.387) and (-0.407 < x[1] < -0.403):
-#         fx1.append(x[0])
-#         fy1.append(x[1])
-#         fz1.append(x[2])
-#
-#         k += 1
-#
-#     if (-0.42 < x[0] < -0.38) and (-0.41 < x[1] < -0.38):
-#         fx2.append(x[0])
-#         fy2.append(x[1])
-#         fz2.append(x[2])
-#
-#         l += 1
-#
-# print(m)
-# print(k)
-# print(l)
+M = 0
+l =0
+
+while l != 5000:
+    stdout.write("\r%d" % k)
+    stdout.flush()
+
+    x = Henon(x, a, b, c)
+
+    fx.append(x[0])
+    fy.append(x[1])
+    fz.append(x[2])
+
+    M += 1
+    # print(m)
+
+    if (-0.15 < x[0] < -0.13) and (-0.15 < x[1] < -0.13):
+        fx1.append(x[0])
+        fy1.append(x[1])
+        fz1.append(x[2])
+
+        k += 1
+
+    if (-0.14 < x[0] < -0.13) and (-0.14 < x[1] < -0.13):
+        fx2.append(x[0])
+        fy2.append(x[1])
+        fz2.append(x[2])
+
+        l += 1
+
+print(' ')
+print(M)
+print(k)
+print(l)
 
 fig, ax = plt.subplots()
-# fig, ax1 = plt.subplots()
-# fig, ax2 = plt.subplots()
+fig, ax1 = plt.subplots()
+fig, ax2 = plt.subplots()
 
-ax.plot(fx, fy, 'g.', ms=0.5)
+ax.plot(fx, fy, 'g.', ms=0.1)
 # ax.plot(fx2, fy2, 'r.', ms=0.1)
 
-# ax1.plot(fx1, fy1, 'g.', ms=0.3)
-# ax2.plot(fx2, fy2, 'g.', ms=0.3)
+ax1.plot(fx1, fy1, 'g.', ms=0.3)
+ax2.plot(fx2, fy2, 'g.', ms=0.3)
 
 ax.set_title(f'A = {a}, B = {b}, C = {c}')
 ax.grid()
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 
-# ax1.set_title(f'A = {a}, B = {b}, C = {c}')
-# ax1.grid()
-# ax1.set_xlabel('X')
-# ax1.set_ylabel('Y')
-#
-# ax2.set_title(f'A = {a}, B = {b}, C = {c}')
-# ax2.grid()
-# ax2.set_xlabel('X')
-# ax2.set_ylabel('Y')
+ax1.set_title(f'A = {a}, B = {b}, C = {c}')
+ax1.grid()
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+
+ax2.set_title(f'A = {a}, B = {b}, C = {c}')
+ax2.grid()
+ax2.set_xlabel('X')
+ax2.set_ylabel('Y')
 
 plt.show()
 # plt.savefig(f'fase_port_a_{a}_c_{c}.eps', format='eps')
