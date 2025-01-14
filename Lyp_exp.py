5f702d303fe789252987a1468e7201c9f89e8eae
davniki <davniki@outlook.com>
2025-01-14 16:28:26 +0300

    changing in the fase_port.py , LE_spectr_cascade.py

---
+++ Lyp_exp.py
@@ -0,0 +1,87 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sys import stdout
+from mpmath import mp
+
+mp.dps = 30
+# Спектр ляпуновских показателей
+
+
+def Henon(x, A, B, C):
+    dx = mp.matrix([[0, 0, 0]])
+    dx[0] = x[1]
+    dx[1] = x[2]
+    dx[2] = B * x[0] + A * x[2] + C * x[1] - x[1] ** 2
+    return dx
+
+
+def d_Henon(dx, x, A, B, C):
+    dx0 = mp.matrix([[0, 0, 0]])
+    dx0[0] = dx[1]
+    dx0[1] = dx[2]
+    dx0[2] = B * dx[0] + (C - 2 * x[1]) * dx[1] + A * dx[2]
+    return dx0
+
+
+A = mp.mpf('1.990915')
+B = mp.mpf('0.5')
+C = mp.mpf('-1.89')
+
+m = 1000
+t = 10
+
+L_1 = 0
+L_2 = 0
+L_3 = 0
+c = []
+
+
+x = mp.matrix([[mp.mpf('0.1'), mp.mpf('0.1'), mp.mpf('0.1')]])
+
+# начальные вектора возмущения, которые ортоганальны и нормированы на еденицу
+dx = mp.matrix([[mp.mpf('1'), 0, 0]])
+dy = mp.matrix([[0, mp.mpf('1'), 0]])
+dz = mp.matrix([[0, 0, mp.mpf('1')]])
+
+sum_1 = 0
+sum_2 = 0
+sum_3 = 0
+
+g_1 = 0
+g_2 = 0
+g_3 = 0
+
+for l in range(100):
+    x = Henon(x, A, B, C)
+
+for j in range(m):
+    for i in range(t):
+        x = Henon(x, A, B, C)
+        dx = d_Henon(dx, x, A, B, C)
+        dy = d_Henon(dy, x, A, B, C)
+        dz = d_Henon(dz, x, A, B, C)
+
+    if mp.norm(dx, 2) != 0:
+        sum_1 += mp.log(mp.norm(dx, 2))
+        g_1 += 1
+        dx = dx / mp.norm(dx, 2)
+
+    dy1 = dy - (dy * dx.T)[0] * dx
+    if mp.norm(dy1, 2) != 0:
+        sum_2 += mp.log(mp.norm(dy1, 2))
+        g_2 += 1
+        dy = dy1 / mp.norm(dy1, 2)
+
+    dz1 = dz - (dz * dx.T)[0] * dx - (dz * dy.T)[0] * dy
+    if mp.norm(dz1, 2) != 0:
+        sum_3 += mp.log(mp.norm(dz1, 2))
+        g_3 += 1
+        dz = dz1 / mp.norm(dz1, 2)
+
+L_1 = sum_1 / (g_1 * t)
+L_2 = sum_2 / (g_2 * t)
+L_3 = sum_3 / (g_3 * t)
+
+print(L_1)
+print(L_2)
+print(L_3)
--- fase_port.py
+++ fase_port.py
@@ -1,150 +1,1 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from sys import stdout
-
-from mpmath import mp
-mp.dps = 30
-
-###
-#  Фазовый портрет динамической системы
-###
-
-
-def Henon(x, A, B, C):
-    dx = mp.matrix([[0, 0, 0]])
-    dx[0] = x[1]
-    dx[1] = x[2]
-    dx[2] = B * x[0] + A * x[2] + C * x[1] - x[1] ** 2
-    return dx
-
-
-# a = mp.mpf('1.42')
-# b = mp.mpf('0.5')
-# c = mp.mpf('-1.7574339')
-
-a = mp.mpf('1.990915')
-b = mp.mpf('0.5')
-c = mp.mpf('-1.89')
-
-# x0 = 0.1
-# y0 = 0.2
-# z0 = 0.3
-
-x = [mp.mpf('0.1'), mp.mpf('0.1'), mp.mpf('0.1')]
-
-fx = []
-fy = []
-fz = []
-
-fx1 = []
-fy1 = []
-fz1 = []
-
-fx2 = []
-fy2 = []
-fz2 = []
-
-T = []
-
-k = 0
-
-for i in range(100):
-    x = Henon(x, a, b, c)
-#
-# for i in range(10_000):
-#     stdout.write("\r%d" % i)
-#     stdout.flush()
-#     x = Henon(x, a, b, c)
-#
-#     fx.append(x[0])
-#     fy.append(x[1])
-#     fz.append(x[2])
-#
-#     k += int(1)
-#
-#     if (-0.393 < x[0] < -0.39) and (-0.4055 < x[1] < -0.4035):
-#         fx1.append(x[0])
-#         fy1.append(x[1])
-#         fz1.append(x[2])
-#
-#         k += 1
-
-# a = mp.mpf('1.42')
-# b = mp.mpf('0.5')
-# c = mp.mpf('-1.757435')
-#
-
-#
-# x = [mp.mpf('0.1'), mp.mpf('0.1'), mp.mpf('0.1')]
-#
-# for i in range(100_000):
-#     x = Henon(x, a, b, c)
-#
-# for i in range(1_000_000):
-#     x = Henon(x, a, b, c)
-#     fx2.append(x[0])
-#     fy2.append(x[1])
-#     fz2.append(x[2])
-
-M = 0
-l =0
-
-while l != 5000:
-    stdout.write("\r%d" % k)
-    stdout.flush()
-
-    x = Henon(x, a, b, c)
-
-    fx.append(x[0])
-    fy.append(x[1])
-    fz.append(x[2])
-
-    M += 1
-    # print(m)
-
-    if (-0.15 < x[0] < -0.13) and (-0.15 < x[1] < -0.13):
-        fx1.append(x[0])
-        fy1.append(x[1])
-        fz1.append(x[2])
-
-        k += 1
-
-    if (-0.14 < x[0] < -0.13) and (-0.14 < x[1] < -0.13):
-        fx2.append(x[0])
-        fy2.append(x[1])
-        fz2.append(x[2])
-
-        l += 1
-
-print(' ')
-print(M)
-print(k)
-print(l)
-
-fig, ax = plt.subplots()
-fig, ax1 = plt.subplots()
-fig, ax2 = plt.subplots()
-
-ax.plot(fx, fy, 'g.', ms=0.1)
-# ax.plot(fx2, fy2, 'r.', ms=0.1)
-
-ax1.plot(fx1, fy1, 'g.', ms=0.3)
-ax2.plot(fx2, fy2, 'g.', ms=0.3)
-
-ax.set_title(f'A = {a}, B = {b}, C = {c}')
-ax.grid()
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-
-ax1.set_title(f'A = {a}, B = {b}, C = {c}')
-ax1.grid()
-ax1.set_xlabel('X')
-ax1.set_ylabel('Y')
-
-ax2.set_title(f'A = {a}, B = {b}, C = {c}')
-ax2.grid()
-ax2.set_xlabel('X')
-ax2.set_ylabel('Y')
-
-plt.show()
-# plt.savefig(f'fase_port_a_{a}_c_{c}.eps', format='eps')
+import matplotlib.pyplot as pltimport numpy as npfrom sys import stdoutfrom mpmath import mpmp.dps = 30####  Фазовый портрет динамической системы###def Henon(x, A, B, C):    dx = mp.matrix([[0, 0, 0]])    dx[0] = x[1]    dx[1] = x[2]    dx[2] = B * x[0] + A * x[2] + C * x[1] - x[1] ** 2    return dx# a = mp.mpf('1.42')# b = mp.mpf('0.5')# c = mp.mpf('-1.7574339')a = mp.mpf('1.990914')b = mp.mpf('0.5')c = mp.mpf('-1.89')# x0 = 0.1# y0 = 0.2# z0 = 0.3x = [mp.mpf('0.1'), mp.mpf('0.1'), mp.mpf('0.1')]fx = []fy = []fz = []fx1 = []fy1 = []fz1 = []fx2 = []fy2 = []fz2 = []T = []k = 0for i in range(100):    x = Henon(x, a, b, c)## for i in range(10_000):#     stdout.write("\r%d" % i)#     stdout.flush()#     x = Henon(x, a, b, c)##     fx.append(x[0])#     fy.append(x[1])#     fz.append(x[2])##     k += int(1)##     if (-0.393 < x[0] < -0.39) and (-0.4055 < x[1] < -0.4035):#         fx1.append(x[0])#         fy1.append(x[1])#         fz1.append(x[2])##         k += 1# a = mp.mpf('1.42')# b = mp.mpf('0.5')# c = mp.mpf('-1.757435')### x = [mp.mpf('0.1'), mp.mpf('0.1'), mp.mpf('0.1')]## for i in range(100_000):#     x = Henon(x, a, b, c)## for i in range(1_000_000):#     x = Henon(x, a, b, c)#     fx2.append(x[0])#     fy2.append(x[1])#     fz2.append(x[2])M = 0l = 0while l != 20_000:    stdout.write("\r%d" % l)    stdout.flush()    x = Henon(x, a, b, c)    fx.append(x[0])    fy.append(x[1])    fz.append(x[2])    M += 1    # print(m)    if (-0.15 < x[0] < -0.13) and (-0.15 < x[1] < -0.13):        fx1.append(x[0])        fy1.append(x[1])        fz1.append(x[2])        k += 1    if (-0.14 < x[0] < -0.13) and (-0.14 < x[1] < -0.13):        fx2.append(x[0])        fy2.append(x[1])        fz2.append(x[2])        l += 1print(' ')print(M)print(k)print(l)fig, ax = plt.subplots()fig, ax1 = plt.subplots()fig, ax2 = plt.subplots()ax.plot(fx, fy, 'g.', ms=0.1)# ax.plot(fx2, fy2, 'r.', ms=0.1)ax1.plot(fx1, fy1, 'g.', ms=0.3)ax2.plot(fx2, fy2, 'g.', ms=0.3)ax.set_title(f'A = {a}, B = {b}, C = {c}')ax.grid()ax.set_xlabel('X')ax.set_ylabel('Y')ax1.set_title(f'A = {a}, B = {b}, C = {c}')ax1.grid()ax1.set_xlabel('X')ax1.set_ylabel('Y')ax2.set_title(f'A = {a}, B = {b}, C = {c}')ax2.grid()ax2.set_xlabel('X')ax2.set_ylabel('Y')plt.show()# plt.savefig(f'fase_port_a_{a}_c_{c}.eps', format='eps')
