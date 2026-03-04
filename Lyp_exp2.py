5a0b26c464b321dfe2f8a15d4b83cb490384d868
davniki <davniki@MacBook-Air-Ula.local>
2026-03-04 11:56:55 +0300

    4 march 2026

--- Lyp_exp2.py
+++ Lyp_exp2.py
@@ -1,7 +1,4 @@
 import numpy as np
-from sys import stdout
-
-
 # Спектр ляпуновских показателей
 
 
@@ -21,29 +18,6 @@ def d_Henon(dx, x, A, B, C):
     return np.array([dx0, dx1, dx2])
 
 
-# m = 2500
-# t = 25
-#
-# L_1 = 0
-# L_2 = 0
-# L_3 = 0
-# c = []
-
-
-# начальные вектора возмущения, которые ортоганальны и нормированы на еденицу
-# dx = np.array([1, 0, 0])
-# dy = np.array([0, 1, 0])
-# dz = np.array([0, 0, 1])
-#
-# sum_1 = 0
-# sum_2 = 0
-# sum_3 = 0
-#
-# g_1 = 0
-# g_2 = 0
-# g_3 = 0
-
-
 def lyp_exp(x, A, B, C):
     dx = np.array([1, 0, 0])
     dy = np.array([0, 1, 0])
@@ -52,9 +26,9 @@ def lyp_exp(x, A, B, C):
     m = 2500
     t = 25
 
-    L_1 = 0
-    L_2 = 0
-    L_3 = 0
+    l_1 = 0
+    l_2 = 0
+    l_3 = 0
     c = []
 
     sum_1 = 0
@@ -69,8 +43,6 @@ def lyp_exp(x, A, B, C):
         x = Henon(x, A, B, C)
 
     for j in range(m):
-        # stdout.write("\r%d" % j)
-        # stdout.flush()
         for i in range(t):
             x = Henon(x, A, B, C)
             dx = d_Henon(dx, x, A, B, C)
@@ -94,21 +66,8 @@ def lyp_exp(x, A, B, C):
             g_3 += 1
             dz = dz1 / np.linalg.norm(dz1, 2)
 
-    L_1 = sum_1 / (g_1 * t)
-    L_2 = sum_2 / (g_2 * t)
-    L_3 = sum_3 / (g_3 * t)
+    l_1 = sum_1 / (g_1 * t)
+    l_2 = sum_2 / (g_2 * t)
+    l_3 = sum_3 / (g_3 * t)
 
-    return L_1, L_2, L_3
-
-# A = 1.43
-# B = 0.5
-# C = -1.75941488
-#
-# x = np.array([0.1, 0.1, 0.1])
-#
-# print(lyp_exp(x, A, B, C))
-
-# print(' ')
-# print(L_1)
-# print(L_2)
-# print(L_3)
+    return l_1, l_2, l_3
--- map_lypexp.py
+++ map_lypexp.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from Lyp_exp2 import lyp_exp, Henon
+from Lyp_exp2 import lyp_exp
 from sys import stdout
 
 delta = 0.01
 
-a_min = -2
-a_max = 2.5
-a_d = (a_max - a_min) / 20
+a_min = 1.67
+a_max = 2
+a_d = (a_max - a_min) / 100
 a_n = np.arange(a_min, a_max + delta, a_d)
 
 b = 0.5
 
-c_min = -2.5
-c_max = 2
-c_d = (c_max - c_min) / 20
+c_min = -1.91
+c_max = -1.83
+c_d = (c_max - c_min) / 100
 c_n = np.arange(c_min, c_max + delta, c_d)
 
 map_Henon = np.zeros((len(a_n), len(c_n)))
@@ -24,16 +24,10 @@ for i, a in enumerate(a_n):
     stdout.flush()
 
     for j, c in enumerate(c_n):
-
-        # stdout.write("\r%d" % j)
-        # stdout.flush()
-        #Пропускаем переходной процесс длины Ntrans
-
-        x = np.array([0.1, 0.1, 0.1])  #начальные условия
+        # начальные условия
+        x = np.array([0.1, 0.1, 0.1])
 
         lexp = lyp_exp(x, a, b, c)
-        # if lexp[0] == 'nan' or lexp[1] == 'nan' or lexp[2] == 'nan':
-        #     map_Henon[i, j] = 0
 
         if lexp[0] < -0.001 and lexp[1] < -0.001 and lexp[2] < -0.001:
             map_Henon[i, j] = 0
@@ -44,14 +38,33 @@ for i, a in enumerate(a_n):
         elif (-0.001 < lexp[0] < 0.001) and (-0.001 < lexp[1] < 0.001) and lexp[2] < -0.001:
             map_Henon[i, j] = 2
 
-        elif lexp[0] > 0.001 and (-0.001 < lexp[1] < 0.001) and lexp[2] < -0.001:
+        elif lexp[0] > 0.001 and lexp[1] < -0.001 and lexp[2] < -0.001:
             map_Henon[i, j] = 3
 
-        else:
+        elif lexp[0] > 0.001 and (-0.001 < lexp[1] < 0.001) and lexp[2] < -0.001:
             map_Henon[i, j] = 4
 
-plt.imshow(map_Henon, cmap='jet', origin='lower',
-           extent=(a_min, a_max, c_min, c_max), aspect=a_d / c_d)
-plt.colorbar()
+        elif lexp[0] > 0.001 and lexp[1] > 0.001 and lexp[2] < -0.001:
+            map_Henon[i, j] = 5
 
+        else:
+            map_Henon[i, j] = 6
+
+print(map_Henon)
+
+plt.imshow(
+    map_Henon.T,
+    cmap = 'jet',
+    origin = 'lower',
+    extent = (a_min, a_max, c_min, c_max),
+    aspect = a_d / c_d
+)
+
+plt.title(f'Карта Ляпуновских показатлей B = {b}')
+plt.xlabel("Параметр A")
+plt.ylabel("Параметр C")
+
+plt.colorbar()
 plt.show()
+
+
